50a76361f1d90f649bb09ab2f1d473e930086be4
Eric <70232980+pogman96@users.noreply.github.com>
2022-04-30 23:03:17 -0400

    initial commit

---
+++ imageProcessing.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import os
+import random
+import cv2
+
+# directory locations
+trainDir = "training/"
+testDir = "testing/"
+allImagesDir = "images/"
+dirs = ["images", "labels"]
+dataDir = "data/"
+yaml_file = "yolov5/data/cellDetection.yaml"
+
+os.chdir(dataDir)
+
+def copyImages(baseDir, finalDir, imgNames):
+    for i in imgNames:
+        originalFile = os.path.join(baseDir, i)
+        outputFile = os.path.join(finalDir, "images", i)
+        original = cv2.imread(originalFile)
+        reSized = cv2.resize(original, SIZE)
+        cv2.imwrite(outputFile, reSized)
+
+
+def writeLabels(imageList, directory):
+    for i in imageList:
+        file = os.path.join(directory, "labels", i.split(".")[0] + ".txt")
+        with open(file, "w") as f:
+            for j in anno[anno["image"] == i].values:
+                f.write(
+                    f"{cellDict[j[5]]} {((j[3]+j[1])/2) / W} {((j[4]+j[2])/2) / H} {(j[3]-j[1]) / W} {(j[4]-j[2]) / W} \n")
+
+# dictionary for cell id's
+cellDict = {"rbc": 0, "wbc": 1}
+cells = list(cellDict.keys())
+
+allImgs = os.listdir("images")
+
+random.shuffle(allImgs)
+
+# spliting images into 2 parts
+training = allImgs[:80]
+testing = allImgs[80:]
+
+# reading annotations for labeling
+anno = pd.read_csv("annotations.csv")
+
+# making folders
+for i in [trainDir, testDir]:
+    for j in dirs:
+        try:
+            os.makedirs(os.path.join(i, j))
+        except FileExistsError:
+            print("Folder already exists")
+
+# getting size of dataset iamges
+temp = cv2.imread("images/"+allImgs[0]).shape
+W = temp[0]
+H = temp[1]
+
+writeLabels(training, trainDir)
+writeLabels(testing, testDir)
+
+# resizing images for YOLOv5
+RESIZE_W = 640
+RESIZE_H = 640
+SIZE = (RESIZE_W, RESIZE_H)
+
+# copying images to training and testing directory
+copyImages(allImagesDir, testDir, testing)
+copyImages(allImagesDir, trainDir, training)
+
+os.chdir("..")
+trainingImagesDir = os.path.join("data", trainDir, "images")
+testingImagesDir = os.path.join("data", testDir, "images")
+
+names = ", ".join([f"\"{i}\"" for i in cells])
+
+# setting up configuration file for training
+with open(yaml_file, "w") as f:
+    f.write(f"train: {trainingImagesDir}\n")
+    f.write(f"val: {testingImagesDir}\n")
+    f.write(f"nc: {len(cells)}\n")
+    f.write(f"names: [{names}]\n")
--- main.py
+++ main.py
@@ -1,49 +1,86 @@
-import pandas as pd
-import cv2
+import torch
 import os
-import matplotlib.pyplot as plt
+import cv2
+import random
+
+#output image dimensions
+RESIZE_W = 1000
+RESIZE_H = 1000
+
+# function to generate output image
+def draw(image:str, labelVals:list, thresh:float):
+    img = cv2.imread(image)
+    img = cv2.resize(img, (RESIZE_W, RESIZE_H))
 
 
-def writeLabels(imageList, directory):
-    for i in imageList:
-        file = os.path.join(directory, "labels", i.split(".")[0] + ".txt")
-        with open(file, "w") as f:
-            for j in anno[anno["image"] == i].values:
-                f.write(
-                    f"{cellDict[j[5]]} {((j[3]+j[1])/2) / W} {((j[4]+j[2])/2) / H} {(j[3]-j[1]) / W} {(j[4]-j[2]) / W} \n")
+    for i in labelVals:
+        # checking confidence threshold
+        if i[-1] < thresh:
+            continue
+        cell = i[0]
+        xc = float(i[1]) * RESIZE_W
+        yc = float(i[2]) * RESIZE_H
+        w = float(i[3]) * RESIZE_W
+        h = float(i[4]) * RESIZE_H
+    
+        img = cv2.rectangle(img, (int(xc - w/2), int(yc - h/2)), (int(xc + w/2), int(yc + h/2)), (0,0,255), 1)
+        img = cv2.putText(img, f"{cell} {round(i[-1], 2)}", (int(xc - w/2), int(yc - h/2 - 5)), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 255), 1)
+        
+    return img
+
+# loading detection model
+model = torch.hub.load("yolov5", "custom", path="yolov5/runs/train/cellDetection/weights/best.pt", source="local")
+
+imgs = os.listdir("data/images")  #allImages
+# sorting for aesthetics
+imgs = sorted(imgs, key=lambda x: int(x.split("-")[-1].split(".")[0]))
 
 
-trainDir = "training/"
-testDir = "testing/"
-dirs = ["images", "labels"]
-dataDir = "data/"
-os.chdir(dataDir)
+while True:
+    usr = input("Classify an image\n1. Enter a specific name\n2. Random select an image\n3. Display all images\n>>> ")
+    if usr == "":
+        break
+    
+    if usr.isnumeric():
+        usr = int(usr)
+        if usr in range(1,4):
+            if usr == 1:
+                name = input("\nFile name (with file extension): ")
 
-cellDict = {"rbc": 0, "wbc": 1}
-cells = list(cellDict.keys())
+            elif usr == 2:
+                name = random.choice(imgs)
 
-allImgs = []
-for i in os.listdir("images"):
-    allImgs.append(i)
+            elif usr == 3:
+                for i in imgs:
+                    print(i)
+                print()
+                continue
+            
+            # getting path to image file
+            path = os.path.join("data/images", name)
 
-training = allImgs[:80]
-testing = allImgs[80:]
+            # getting image dimensions
+            imgDimensions = cv2.imread(path).shape
+            W = imgDimensions[0]
+            H = imgDimensions[1]
 
-anno = pd.read_csv("annotations.csv")
+            # applying trained model to image
+            res = model(path)
 
-for i in [trainDir, testDir]:
-    for j in dirs:
-        try:
-            os.makedirs(os.path.join(i, j))
-        except FileExistsError:
-            print("Folder already exists")
+            # results processing
+            values = []
+            for i in res.pandas().xyxy[0].values:
+                temp = [i[-1], ((i[0] + i[2])/2) / W, ((i[1] + i[3])/2) / H, (i[2]-i[0]) / W, (i[3] - i[1]) / H, i[-3]]
+                values.append(temp)
 
-temp = cv2.imread("images/"+allImgs[0]).shape
-W = temp[0]
-H = temp[1]
+            # display image
+            image = draw(path, values, 0.7)
+            cv2.imshow("result", image)
+            cv2.waitKey(0)
 
-writeLabels(training, trainDir)
-writeLabels(testing, testDir)
+        else:
+            continue
 
-RESIZE_W = 640
-RESIZE_H = 640
+    else:
+        continue
+    
